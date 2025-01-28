a09419a8ef700963a1ffb6bbc04019620cde56c8
Prit Vanjara <81341654+DroopyPirate@users.noreply.github.com>
2025-01-28 11:50:15 -0700

    LC796

---
+++ LeetCode/Strings and Arrays/Others/LC-796 Rotate String/Program.cs
@@ -0,0 +1,20 @@
+﻿class LC796
+{
+    public static bool RotateString(string s, string goal)
+    {
+        if (s.Length != goal.Length)
+        {
+            return false;
+        }
+        string doubled = s + s;
+        
+        return doubled.Contains(goal);
+    }
+
+    public static void Main(string[] args)
+    {
+        string s = "abcde";
+        string goal = "cdeab";
+        Console.WriteLine(RotateString(s, goal)); // Output: True
+    }
+}
