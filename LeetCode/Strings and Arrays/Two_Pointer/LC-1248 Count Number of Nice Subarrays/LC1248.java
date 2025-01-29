a39e429497c8ef3493ef953ecf43316f24b6099d
Prit Vanjara <81341654+DroopyPirate@users.noreply.github.com>
2025-01-29 11:26:46 -0700

    LC1248

---
+++ LeetCode/Strings and Arrays/Two_Pointer/LC-1248 Count Number of Nice Subarrays/LC1248.java
@@ -0,0 +1,32 @@
+class LC1248 {
+    public static int numberOfSubarrays(int[] nums, int k) {
+        int currentOdds = 0, left = 0, right = 0, result = 0, numsLength = nums.length, evenBeforeLeft = 0;
+
+        while(right < numsLength){
+            //If odd then increase Odds count and reset evenBeforeLeft
+            if(nums[right] % 2 == 1){
+                currentOdds++;
+                evenBeforeLeft = 0;
+            }
+            
+            //While k odds are found, increase left pointer and decrease Odds count
+            //Also increase evenBeforeLeft if left pointer is even
+            while(currentOdds == k){
+                evenBeforeLeft++;
+                if(nums[left] % 2 == 1) currentOdds--;
+                left++;
+            }
+
+            //Add evenBeforeLeft to result for each right pointer even number found
+            result += evenBeforeLeft;
+            right++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,1,1};
+        int k = 3;
+        System.out.println(numberOfSubarrays(nums, k));
+    }
+}
