a15e04aa24027116420d756a15452a5201a61f22
Prit Vanjara <81341654+DroopyPirate@users.noreply.github.com>
2025-02-13 10:31:42 -0700

    LC739

---
+++ LeetCode/Stacks and Queues/Basic Stack Problmes/LC-739 Daily Temperatures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LC739
+{
+    public static int[] DailyTemperatures(int[] temperatures)
+    {
+        int[] result = new int[temperatures.Length];
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
+            {
+                int index = stack.Pop();
+                result[index] = i - index;
+            }
+            stack.Push(i);
+        }
+        return result;
+    }
+
+    public static void Main()
+    {
+        int[] temperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
+        int[] result = DailyTemperatures(temperatures);
+        
+        foreach (int i in result)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
